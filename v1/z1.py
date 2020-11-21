90c6ab80347b6d28c4c1320431ecbd35ea9bf1a7
Dejan <dejan@pop-os.localdomain>
2020-11-21 12:07:52 +0100

    first commit

---
+++ v1/z1.py
@@ -0,0 +1,38 @@
+import math
+
+def generate(n):
+	list = []
+	for i in range(1, n + 1):
+		list.append(i)
+	return list
+
+def is_prime(x):
+	for i in range(0, int(math.sqrt(x)) + 1):
+		if i < 2:
+			continue
+				
+		if x % i == 0:
+			return False
+	return True
+
+def primes(list):
+	primes = []
+	for x in list:
+	
+		if x < 2:
+			continue
+			
+		if is_prime(x):
+			primes.append(x)
+			
+	return primes			
+
+def main():
+	list = generate(30)
+	print(list)
+	list = primes(list)
+	print (list)
+	
+
+if __name__=="__main__":
+	main()
